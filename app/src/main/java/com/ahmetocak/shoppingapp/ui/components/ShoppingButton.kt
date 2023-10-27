f2000cc1a764bbf75fc3bf26e4bdf7a5774e6280
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-10-27 21:32:56 +0300

    bug fixed

--- app/src/main/java/com/ahmetocak/shoppingapp/ui/components/ShoppingButton.kt
+++ app/src/main/java/com/ahmetocak/shoppingapp/ui/components/ShoppingButton.kt
@@ -2,7 +2,6 @@ package com.ahmetocak.shoppingapp.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -10,7 +9,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import com.ahmetocak.shoppingapp.R
 
 @Composable
@@ -19,7 +17,7 @@ fun ShoppingButton(modifier: Modifier = Modifier, onClick: () -> Unit, buttonTex
         modifier = modifier
             .fillMaxWidth()
             .height(dimensionResource(id = R.dimen.button_height)),
-        onClick = { /*TODO*/ },
+        onClick = onClick,
         shape = RoundedCornerShape(dimensionResource(id = R.dimen.button_corner_size))
     ) {
         Text(text = buttonText, style = MaterialTheme.typography.titleMedium)
