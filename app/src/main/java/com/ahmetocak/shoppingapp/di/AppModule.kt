dc8ff338482b62d51817bdba6db7e1ee4b7ef49a
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-11-01 22:22:31 +0300

    firebase auth di code moved to AppModule.kt

--- app/src/main/java/com/ahmetocak/shoppingapp/di/AppModule.kt
+++ app/src/main/java/com/ahmetocak/shoppingapp/di/AppModule.kt
@@ -3,6 +3,9 @@ package com.ahmetocak.shoppingapp.di
 import android.content.Context
 import android.content.SharedPreferences
 import com.ahmetocak.shoppingapp.utils.USER_REMEMBER_ME_KEY
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,5 +21,11 @@ object AppModule {
     @Singleton
     fun provideSharedPreferences(@ApplicationContext context: Context) : SharedPreferences {
         return context.getSharedPreferences(USER_REMEMBER_ME_KEY, Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth() : FirebaseAuth {
+        return Firebase.auth
     }
 }
--- app/src/main/java/com/ahmetocak/shoppingapp/di/DataSourceModule.kt
+++ app/src/main/java/com/ahmetocak/shoppingapp/di/DataSourceModule.kt
@@ -2,9 +2,7 @@ package com.ahmetocak.shoppingapp.di
 
 import com.ahmetocak.shoppingapp.data.datasource.remote.firebase.FirebaseRemoteDataSource
 import com.ahmetocak.shoppingapp.data.datasource.remote.firebase.FirebaseRemoteDatasourceImpl
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,12 +12,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DataSourceModule {
-
-    @Provides
-    @Singleton
-    fun provideFirebaseAuth() : FirebaseAuth {
-        return Firebase.auth
-    }
 
     @Provides
     @Singleton
