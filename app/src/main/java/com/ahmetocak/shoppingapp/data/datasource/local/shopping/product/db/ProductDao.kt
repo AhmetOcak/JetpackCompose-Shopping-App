f317588f1b2d31579fda21143bec3a87d160f2ef
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-11-13 12:56:11 +0300

    bug fixed

--- app/src/main/java/com/ahmetocak/shoppingapp/data/datasource/local/shopping/product/db/ProductDao.kt
+++ app/src/main/java/com/ahmetocak/shoppingapp/data/datasource/local/shopping/product/db/ProductDao.kt
@@ -2,13 +2,14 @@ package com.ahmetocak.shoppingapp.data.datasource.local.shopping.product.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ahmetocak.shoppingapp.model.shopping.ProductEntity
 
 @Dao
 interface ProductDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addProduct(productEntity: ProductEntity)
 
     @Query("SELECT * FROM ProductEntity")
