cb0b7dad8c438a3fe6db32c6a2ad13bbf52be947
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-11-13 22:34:59 +0300

    cart empty view added

--- app/src/main/java/com/ahmetocak/shoppingapp/presentation/cart/CartScreen.kt
+++ app/src/main/java/com/ahmetocak/shoppingapp/presentation/cart/CartScreen.kt
@@ -2,6 +2,7 @@ package com.ahmetocak.shoppingapp.presentation.cart
 
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -37,12 +38,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -179,33 +183,37 @@ private fun CartList(
     onIncreaseClicked: (Int) -> Unit,
     onDecreaseClicked: (Int) -> Unit
 ) {
-    LazyVerticalGrid(
-        modifier = modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.verticalGradient(
-                    listOf(
-                        colorResource(id = R.color.mauve),
-                        colorResource(id = R.color.pale_purple),
-                    ),
+    if (cartList.isNotEmpty()) {
+        LazyVerticalGrid(
+            modifier = modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        listOf(
+                            colorResource(id = R.color.mauve),
+                            colorResource(id = R.color.pale_purple),
+                        ),
+                    )
+                ),
+            columns = GridCells.Fixed(1),
+            contentPadding = PaddingValues(dimensionResource(id = R.dimen.two_level_margin)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.one_level_margin))
+        ) {
+            items(cartList) { cart ->
+                CartItem(
+                    id = cart.id,
+                    imageUrl = cart.image,
+                    title = cart.title,
+                    price = cart.price * cart.count,
+                    onRemoveItemClick = onRemoveItemClick,
+                    itemCount = cart.count,
+                    onIncreaseClicked = onIncreaseClicked,
+                    onDecreaseClicked = onDecreaseClicked
                 )
-            ),
-        columns = GridCells.Fixed(1),
-        contentPadding = PaddingValues(dimensionResource(id = R.dimen.two_level_margin)),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.one_level_margin))
-    ) {
-        items(cartList) { cart ->
-            CartItem(
-                id = cart.id,
-                imageUrl = cart.image,
-                title = cart.title,
-                price = cart.price * cart.count,
-                onRemoveItemClick = onRemoveItemClick,
-                itemCount = cart.count,
-                onIncreaseClicked = onIncreaseClicked,
-                onDecreaseClicked = onDecreaseClicked
-            )
+            }
         }
+    } else {
+        EmptyCartListView(modifier = modifier, messageId = R.string.cart_empty)
     }
 }
 
@@ -326,6 +334,34 @@ private fun CartItemImg(imageUrl: String) {
                 .build(),
             contentDescription = null,
             contentScale = ContentScale.Fit
+        )
+    }
+}
+
+@Composable
+private fun EmptyCartListView(
+    modifier: Modifier,
+    messageId: Int,
+    imageSize: Dp = 112.dp
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier.size(imageSize),
+            painter = painterResource(id = R.drawable.search_result_empty),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = dimensionResource(id = R.dimen.two_level_margin))
+                .padding(horizontal = dimensionResource(id = R.dimen.four_level_margin)),
+            text = stringResource(id = messageId),
+            textAlign = TextAlign.Center
         )
     }
 }
