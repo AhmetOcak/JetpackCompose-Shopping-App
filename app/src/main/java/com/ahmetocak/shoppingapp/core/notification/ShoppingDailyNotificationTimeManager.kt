fa3438459a1d021da38ac84d19d5a5ff790f1128
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-11-23 21:25:13 +0300

    daily notification time set

--- app/src/main/java/com/ahmetocak/shoppingapp/core/notification/ShoppingDailyNotificationTimeManager.kt
+++ app/src/main/java/com/ahmetocak/shoppingapp/core/notification/ShoppingDailyNotificationTimeManager.kt
@@ -2,5 +2,5 @@ package com.ahmetocak.shoppingapp.core.notification
 
 object ShoppingDailyNotificationTimeManager {
     const val REMINDER_HOUR = 20
-    const val REMINDER_MINUTES = 22
+    const val REMINDER_MINUTES = 0
 }
